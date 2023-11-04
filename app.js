524daf6eaf2ef06aca672198ae5dd720cb5bf2e5
Mustfa1122 <145039363+Mustfa1122@users.noreply.github.com>
2023-11-04 06:13:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var firstName="Mark"
+var lastName="myers"
+var fullName=firstName + lastName;
+if(fullName==="Mark"+""+"Myers"){
+    alert("your answer is right")}
+    else
+    {
+    alert("your answer is wrong")
+    }
